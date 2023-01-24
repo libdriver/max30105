92b8872d58fb9a2ab12a2a04bc43d7421454441c
e <lishifenging@outlook.com>
2023-01-24 18:29:00 +0800

    style: change the code style

--- project/raspberrypi4b/src/main.c
+++ project/raspberrypi4b/src/main.c
@@ -118,9 +118,9 @@ void max30105_receive_callback(uint8_t type)
  * @param[in] argc is arg numbers
  * @param[in] **argv is the arg address
  * @return    status code
- *             - 0 success
- *             - 1 run failed
- *             - 5 param is invalid
+ *            - 0 success
+ *            - 1 run failed
+ *            - 5 param is invalid
  * @note      none
  */
 uint8_t max30105(uint8_t argc, char **argv)
